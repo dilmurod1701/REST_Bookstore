36f16a0e41e5e0f5d7efa3996e4e587f5a6cd101
dilmurod1701 <139139677+dilmurod1701@users.noreply.github.com>
2023-11-11 13:14:55 +0500

    Updated: urls.py

--- api/urls.py
+++ api/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
     path('<int:id>/delete', views.DeleteBook.as_view(), name='delete'),
     path('<int:id>/update', views.UpdateBook.as_view(), name='update'),
     path('<str:username>', views.ReturnBookByOwner.as_view(), name='return_book_by_owns'),
+    path('openapi', get_schema_view(title="openapi", description="API for all things …", version="3.0.0"), name='openapi-schema'),
     path('swagger-ui/', TemplateView.as_view(template_name='api/swagger-ui.html', extra_context={'schema_url': 'openapi-schema'}), name='swagger-ui'),
 ]
